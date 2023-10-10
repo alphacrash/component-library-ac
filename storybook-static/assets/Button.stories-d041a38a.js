94754610bf6b6cc9f13bb96852307bb33383baaa
alphacrash <sudhansu.ace@gmail.com>
2023-10-10 17:03:41 +0530

    Initial Commit

---
+++ storybook-static/assets/Button.stories-d041a38a.js
@@ -0,0 +1,27 @@
+import{B as k}from"./Button-040b9ad5.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-8d47fad6.js";const D={title:"Example/Button",component:k,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}}},r={args:{primary:!0,label:"Button"}},e={args:{label:"Button"}},a={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}},s={args:{primary:!0,label:"Delete now",backgroundColor:"red"}};var t,n,c;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: "Button"
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var l,m,u;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: "Button"
+  }
+}`,...(u=(m=e.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var p,i,d;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    label: "Button"
+  }
+}`,...(d=(i=a.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var g,b,B;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    label: "Button"
+  }
+}`,...(B=(b=o.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};var y,S,z;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: "Delete now",
+    backgroundColor: "red"
+  }
+}`,...(z=(S=s.parameters)==null?void 0:S.docs)==null?void 0:z.source}}};const E=["Primary","Secondary","Large","Small","Warning"];export{a as Large,r as Primary,e as Secondary,o as Small,s as Warning,E as __namedExportsOrder,D as default};
+//# sourceMappingURL=Button.stories-d041a38a.js.map
